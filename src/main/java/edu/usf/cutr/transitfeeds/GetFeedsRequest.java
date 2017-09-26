27d5a02a1c09750cbeaf5ef7fc409abff72d7db9
barbeau <barbeau@cutr.usf.edu>
2017-09-26 15:27:33 -0400

    Add all request parameters

--- src/main/java/edu/usf/cutr/transitfeeds/GetFeedsRequest.java
+++ src/main/java/edu/usf/cutr/transitfeeds/GetFeedsRequest.java
@@ -63,7 +63,10 @@ public class GetFeedsRequest {
     public static void main(String[] args) {
         try {
             String apiKey = "76edc18d-54d4-4132-9f53-e8e25be976e7";
-            GetFeedsResponse response = new GetFeedsRequest.Builder(apiKey).build().call();
+            GetFeedsResponse response = new GetFeedsRequest.Builder(apiKey)
+                    .setType("gtfsrealtime")
+                    .build()
+                    .call();
             System.out.println(response.toString());
         } catch (IOException e) {
             e.printStackTrace();
@@ -93,17 +96,13 @@ public class GetFeedsRequest {
         private String mApiEndPoint = "http://api.transitfeeds.com/v1/getFeeds";
         private String mApiKey;
         private String mLocation;
-        private String mSources;
-        private Double mFocusPointLat;
-        private Double mFocusPointLon;
-        private Double mBoundaryMinLat;
-        private Double mBoundaryMinLon;
-        private Double mBoundaryMaxLat;
-        private Double mBoundaryMaxLon;
-        private Integer mSize;
+        private String mDescendants;
+        private Integer mPage;
+        private Integer mLimit;
+        private String mType;
 
         /**
-         * A Builder for making a request to the Pelias Search API
+         * A Builder for making a request to the TransitFeeds.com GetFeeds API
          *
          * @param apiKey the API key to be used in the request
          */
@@ -112,7 +111,7 @@ public class GetFeedsRequest {
         }
 
         /**
-         * Sets the API endpoint that will be used for the API call.  By default https://search.mapzen.com/v1/search
+         * Sets the API endpoint that will be used for the API call.  By default http://api.transitfeeds.com/v1/getFeeds
          * will be used.
          *
          * @param apiEndpoint the API endpoint to be used in the API call
@@ -124,6 +123,67 @@ public class GetFeedsRequest {
         }
 
         /**
+         * Sets the unique ID of a location. If specified, feeds will only be returned that belong to this location (and perhaps sub-locations too, depending on the descendants value). You can use the /getLocations API endpoint to determine location IDs.
+         *
+         * @param location the unique ID of a location. If specified, feeds will only be returned that belong to this location (and perhaps sub-locations too, depending on the descendants value). You can use the /getLocations API endpoint to determine location IDs.
+         * @return this same Builder so Builder calls can be chained
+         */
+        public Builder setLocation(String location) {
+            mLocation = location;
+            return this;
+        }
+
+        /**
+         * If a location is specified in location, this flag can be used to control if returned feeds must be assigned directly to the location, or if feeds belonging to sub-locations can also be returned. If 0, then feeds must be assigned directly to the specified location.
+         * <p>
+         * Default is 1 if parameter isn't provided.
+         *
+         * @param descendants if a location is specified in location, this flag can be used to control if returned feeds must be assigned directly to the location, or if feeds belonging to sub-locations can also be returned. If 0, then feeds must be assigned directly to the specified location.
+         * @return this same Builder so Builder calls can be chained
+         */
+        public Builder setDescendants(String descendants) {
+            mDescendants = descendants;
+            return this;
+        }
+
+        /**
+         * Sets the page number of results to return. For example, if you specify a page of 2 with a limit of 10, then results 11-20 are returned. The number of pages available is included in the response.
+         *
+         * @param page the page number of results to return. For example, if you specify a page of 2 with a limit of 10, then results 11-20 are returned. The number of pages available is included in the response.
+         * @return this same Builder so Builder calls can be chained
+         */
+        public Builder setPage(int page) {
+            mPage = page;
+            return this;
+        }
+
+        /**
+         * Sets the maximum number of results to return.
+         *
+         * @param limit the maximum number of results to return.
+         * @return this same Builder so Builder calls can be chained
+         */
+        public Builder setLimit(int limit) {
+            mLimit = limit;
+            return this;
+        }
+
+        /**
+         * Sets the type of feeds to return. If unspecified, feeds of all types are returned.
+         * <p>
+         * Known valid values are:
+         * gtfs
+         * gtfsrealtime
+         *
+         * @param type the type of feeds to return
+         * @return this same Builder so Builder calls can be chained
+         */
+        public Builder setType(String type) {
+            mType = type;
+            return this;
+        }
+
+        /**
          * Builds the GetFeedsRequest using the specified parameters
          *
          * @return the GetFeedsRequest using the specified parameters
@@ -131,37 +191,32 @@ public class GetFeedsRequest {
         public GetFeedsRequest build() {
             StringBuilder builder = new StringBuilder();
             builder.append(mApiEndPoint);
-            builder.append("?text=");
-            builder.append(mLocation);
-            builder.append("&key=");
+            builder.append("?key=");
             builder.append(mApiKey);
 
-            if (mSources != null) {
-                builder.append("&sources=");
-                builder.append(mSources);
+            if (mLocation != null) {
+                builder.append("&location=");
+                builder.append(mLocation);
             }
 
-            if (mSize != null) {
-                builder.append("&size=");
-                builder.append(mSize);
+            if (mDescendants != null) {
+                builder.append("&descendants=");
+                builder.append(mDescendants);
             }
 
-            if (mFocusPointLat != null && mFocusPointLon != null) {
-                builder.append("&focus.point.lat=");
-                builder.append(mFocusPointLat);
-                builder.append("&focus.point.lon=");
-                builder.append(mFocusPointLon);
+            if (mPage != null) {
+                builder.append("&page=");
+                builder.append(mPage);
             }
 
-            if (mBoundaryMinLat != null) {
-                builder.append("&boundary.rect.min_lat=");
-                builder.append(mBoundaryMinLat);
-                builder.append("&boundary.rect.min_lon=");
-                builder.append(mBoundaryMinLon);
-                builder.append("&boundary.rect.max_lat=");
-                builder.append(mBoundaryMaxLat);
-                builder.append("&boundary.rect.max_lon=");
-                builder.append(mBoundaryMaxLon);
+            if (mLimit != null) {
+                builder.append("&limit=");
+                builder.append(mLimit);
+            }
+
+            if (mType != null) {
+                builder.append("&type=");
+                builder.append(mType);
             }
 
             return new GetFeedsRequest(builder.toString());
